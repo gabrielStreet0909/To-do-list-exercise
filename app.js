d2cfaa27b5532bad595bdf43e56e7b1a7e4f64da
Gabriel Alves <biel08718@gmail.com>
2023-11-16 09:28:06 -0300

    Aprendendo comandos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const express = require('express')
+const app = express()
+
+// para setarmos rotas fazemos da seguinte maneira -->
+
+app.get('/', (req, res) => {
+  res.send('<h1>Minha lista de tarefas</h1>')
+})
+
+// como devolver respostas JSON
+
+app.get('/json', (req, res) => {
+  res.json({title: 'Tarefa X', done: true})
+})
+
+app.listen('3000', () => {
+  console.log('Servidor foi iniciado!!')
+})
